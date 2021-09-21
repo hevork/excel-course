23b3d925689af8d166fd9c563b48fb608eb7a377
геворк <70849404+hevork@users.noreply.github.com>
2021-09-21 16:54:21 +0300

    Finish project config

---
+++ src/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    parser: "@babel/eslint-parser",
+    parserOptions: {
+      sourceType: "module",
+      allowImportExportEverywhere: false,
+      ecmaFeatures: {
+        globalReturn: false,
+      },
+      babelOptions: {
+        configFile: "./babel.config.json",
+      },
+    },
+    env:{
+        browser:true,
+        node:true,
+        es6:true
+    },
+    extends: ["eslint:recommended", "google"],
+    rules:{
+        'semi':'off',
+        'comma-dangle':'off'
+    }
+  };
